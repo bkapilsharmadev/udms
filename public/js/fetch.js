7ad6f2010451d56203e109fd2b76bba79c62726b
harshalnmims <harshal.nanekar.EXT@nmims.edu>
2024-11-14 22:15:37 +0530

    login changes

---
+++ public/js/fetch.js
@@ -0,0 +1,43 @@
+// Utility function to make API requests with fetch
+async function fetchApi(url, method = "GET", data = null) {
+  const options = {
+    method,
+    headers: {
+      Accept: "application/json",
+    },
+  };
+
+  if (method === "POST" && data) {
+    if (data instanceof FormData) {
+      options.body = data;
+    } else {
+      options.headers["Content-Type"] = "application/json";
+      options.body = JSON.stringify(data);
+    }
+  }
+
+  try {
+    const response = await fetch(url, options);
+
+    if (response.status == 401) {
+      if (location.pathname === "/signin") {
+        return { status: 401, message: "Invalid Credentials !" };
+      } else {
+        location.href = "/signin";
+        return;
+      }
+    }
+
+    if (response.ok) {
+      const contentType = response.headers.get("Content-Type");
+      if (contentType && contentType.includes("application/json")) {
+        return await response.json();
+      } else {
+        return await response.text();
+      }
+    }
+  } catch (error) {
+    console.error("Fetch API error:", error);
+    throw error;
+  }
+}
