9cec3a41c0f0185900722422f7aea7609a669463
Kapil Sharma <bkapilsharma.dev@gmail.com>
2024-11-19 14:37:42 +0530

    dashboard added

--- app/routes/dashboard.route.js
+++ app/routes/dashboard.route.js
@@ -1,9 +1,8 @@
 const router = require("express").Router();
 
-const { asyncErrorHandler } = require('../middleware/error.middleware');
-const dashboardController = require('../controllers/dashboard.controller');
+const { asyncErrorHandler } = require("../middleware/error.middleware");
+const dashboardController = require("../controllers/dashboard.controller");
 
-router.get("/",asyncErrorHandler(dashboardController.renderDashboard));
+router.get("/", asyncErrorHandler(dashboardController.renderDashboard));
 
-module.exports = router;  
-
+module.exports = router;
--- test.js
+++ test.js
@@ -1,1 +1,18 @@
-console.log(this);
+class CustomError extends Error {
+    constructor(message) {
+      super(message); // Call the parent Error constructor
+      this.name = this.constructor.name; // Set the error name to the class name
+      Error.captureStackTrace(this); // Do NOT exclude the constructor
+    }
+  }
+  
+  function helperFunction() {
+    throw new CustomError("Something went wrong");
+  }
+  
+  try {
+    helperFunction();
+  } catch (error) {
+    console.log(error.stack);
+  }
+  
